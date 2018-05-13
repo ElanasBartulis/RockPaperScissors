2f8accae4f63400a0b4cc4c1ca61b35ab3ae6990
ElanasBartulis <elanasbar@gmail.com>
2018-05-13 16:59:24 +0300

    v2

--- first_program.py
+++ first_program.py
@@ -1,51 +1,57 @@
 import random
+player_wins = 0
+computer_wins = 0
+score = 3
 
-print("...rock...")
-print("...paper...")
-print("...scissors...")
+while player_wins < score and computer_wins < score:
+	print(f"Player score : {player_wins} Computer score {computer_wins}")
+	print("...rock...")
+	print("...paper...")
+	print("...scissors...")
 
-player = input("enter Player choice: ").lower()
-random_num = random.randint(0,2)
+	player = input("Enter Something: ").lower()
+	if player == "quit" or player == "q":
+		break
+	random_num = random.randint(0,2)
 
-#Comp logika
-if random_num == 0:
-	computer = "rock"
-elif random_num == 1:
-	computer = "paper"
+	#Comp logika
+	if random_num == 0:
+		computer = "rock"
+	elif random_num == 1:
+		computer = "paper"
+	else:
+		computer = "scissors"
+	print(f"Computer showed ----> {computer} <----")
+
+	if player == computer:
+		print("It's a draw")
+	elif player == "rock":
+		if computer == "scissors":
+			print("Player Wins!")
+			player_wins += 1
+		else:
+			print("Computer Wins!")
+			computer_wins += 1
+	elif player == "scissors":
+		if computer == "paper":
+			print("Player Wins!")
+			player_wins += 1
+		else:
+			print("Computer Wins!")
+			computer_wins += 1
+	elif player == "paper":
+		if computer == "rock":
+			print("Player Wins!")
+			player_wins += 1
+		else:
+			print("Computer Wins!")
+			computer_wins += 1
+	else:
+		print("Something went wrong!")
+
+if player_wins > computer_wins:
+	print("CONGRATS , YOU WON!")
+elif player_wins == computer_wins:
+	print("IT'S A TIE")
 else:
-	computer = "scissors"
-print(f"Computer showed ----> {computer} <----")
-
-if player == computer:
-	print("It's a draw")
-elif player == "rock":
-	if computer == "scissors":
-		print("Player Wins!")
-	else:
-		print("Computer Wins!")
-elif player == "scissors":
-	if computer == "paper":
-		print("Player Wins!")
-	else:
-		print("Computer Wins!")
-elif player == "paper":
-	if computer == "rock":
-		print("Player Wins!")
-	else:
-		print("Computer Wins!")
-else:
-	print("Something went wrong!") 
-
-#	p1 = input("Player 1: rock, paper, or scissors ")
-#p2 = input("Player 2: rock, paper, or scissors ")
- 
-#if p1 == p2:
-#    print("Draw")
-#elif p1 == "rock" and p2 == "scissors":
-#   print("p1 wins")
-#elif p1 == "paper" and p2 == "rock":
-#    print("p1 wins")
-#elif p1 == "scissors" and p2 == "paper":
-#    print("p1 wins")
-#else:
-#    print("p2 wins")
+	print("BUMMER , YOU LOST :(")
